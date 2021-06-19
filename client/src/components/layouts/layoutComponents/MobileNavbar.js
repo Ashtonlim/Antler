baf3a9a888f235480145bd32091ce664f8b056ed
ashtonlim <ashtonlim22@gmail.com>
2021-06-19 20:17:32 +0800

    add symbol file and AC component

--- client/src/components/layouts/layoutComponents/MobileNavbar.js
+++ client/src/components/layouts/layoutComponents/MobileNavbar.js
@@ -1,30 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { Link, useLocation } from "react-router-dom";
 import {
   HomeOutlined,
   PieChartOutlined,
-  SettingOutlined,
+  StockOutlined,
   UserOutlined,
   FileOutlined,
 } from "@ant-design/icons";
+
+import GC from "context";
 
 const menu = [
   { icon: <HomeOutlined />, tab: "Home" },
   { icon: <FileOutlined />, tab: "Feed" },
   { icon: <PieChartOutlined />, tab: "Markets" },
   { icon: <UserOutlined />, tab: "Account" },
-  { icon: <SettingOutlined />, tab: "Settings" },
+  { icon: <StockOutlined />, tab: "Settings" },
 ];
 
 const MobileNavbar = () => {
-  const [sel, setSel] = useState("Home");
+  const { pathname } = useLocation();
+  const [sel, setSel] = useState(pathname);
+
   const handleClick = (e) => {
     setSel(e.currentTarget.innerText);
   };
+
+  const { state } = useContext(GC);
   return (
     <section className="mobileNavbar">
       <div className="container">
         <nav className="bottom-nav">
-          {menu &&
+          {/* {menu &&
             menu.map(({ icon, tab }, key) => (
               <div
                 key={key}
@@ -36,7 +43,81 @@ const MobileNavbar = () => {
                   <span>{tab}</span>
                 </div>
               </div>
-            ))}
+            ))} */}
+          <nav className="bottom-nav">
+            <Link
+              onClick={handleClick}
+              className={`bottom-nav-item ${sel === "/" ? "active" : ""}`}
+              to={`/`}
+            >
+              <div className="bottom-nav-link">
+                <HomeOutlined />
+                <span>Home</span>
+              </div>
+            </Link>
+            <Link
+              onClick={handleClick}
+              className={`bottom-nav-item ${sel === "/feed" ? "active" : ""}`}
+              to={`/feed`}
+            >
+              <div className="bottom-nav-link">
+                <FileOutlined />
+                <span>Feed</span>
+              </div>
+            </Link>
+            <Link
+              onClick={handleClick}
+              className={`bottom-nav-item ${
+                sel === "/markets" ? "active" : ""
+              }`}
+              to={`/markets`}
+            >
+              <div className="bottom-nav-link">
+                <StockOutlined />
+                <span>Markets</span>
+              </div>
+            </Link>
+            <Link
+              onClick={handleClick}
+              className={`bottom-nav-item ${
+                sel === "/portfolio" ? "active" : ""
+              }`}
+              to={`/portfolio`}
+            >
+              <div className="bottom-nav-link">
+                <PieChartOutlined />
+                <span>Portfolio</span>
+              </div>
+            </Link>
+            {state.loggedIn && (
+              <Link
+                onClick={handleClick}
+                className={`bottom-nav-item ${
+                  sel.includes("/profile") ? "active" : ""
+                }`}
+                to={`/profile/${state.userObj.name}`}
+              >
+                <div className="bottom-nav-link">
+                  <UserOutlined />
+                  <span>Profile</span>
+                </div>
+              </Link>
+            )}
+            {!state.loggedIn && (
+              <Link
+                onClick={handleClick}
+                className={`bottom-nav-item ${
+                  sel === "/login" ? "active" : ""
+                }`}
+                to={`/login`}
+              >
+                <div className="bottom-nav-link">
+                  <UserOutlined />
+                  <span>LogIn</span>
+                </div>
+              </Link>
+            )}
+          </nav>
         </nav>
       </div>
     </section>
