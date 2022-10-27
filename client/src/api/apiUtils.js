e656747c045f99522dd6cd023e9bfeb86cdc8a97
ashtonlim <ashtonlim22@gmail.com>
2022-10-27 15:49:28 +0800

    m

--- client/src/api/apiUtils.js
+++ client/src/api/apiUtils.js
@@ -1,12 +1,12 @@
-import { resHandler } from "./factory";
+import { resHandler } from './factory'
 
-const { REACT_APP_AUTH } = process.env;
+const { REACT_APP_AUTH } = process.env
 
-const forex = `api/v7`;
-const BASE = REACT_APP_AUTH;
-const key = "e4522a915b50a3abd0b7";
+const forex = `api/v7`
+const BASE = REACT_APP_AUTH
+const key = 'e4522a915b50a3abd0b7'
 
-export const currConv = async ({ from = "USD", to = "SGD" } = {}) => {
-  const api = `${BASE}/${forex}/convert?q=${from}_${to}&compact=ultra&apiKey=${key}`;
-  return await resHandler(await fetch(api));
-};
+export const currConv = async ({ from = 'USD', to = 'SGD' } = {}) => {
+  const api = `${BASE}/${forex}/convert?q=${from}_${to}&compact=ultra&apiKey=${key}`
+  return await resHandler(await fetch(api))
+}
